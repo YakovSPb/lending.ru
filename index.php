502eef8ba71de46c98d2ae602ba911ca2b5a6edc
Yakov <yakov_dux@bk.ru>
2018-12-11 20:39:42 +0400

    Hedear in work

---
+++ index.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Lending</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700" rel="stylesheet">
+    <!-- CSS -->
+    <link type="text/css" rel="stylesheet" href="css/main.css">
+    <link type="text/css" rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body>
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-2">
+                <div class="logo">FitApp</div>
+            </div>
+             <div class="col-lg-6 offset-lg-2">
+                <nav>
+                    <ul class="top_menu">
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Features</a></li>
+                        <li><a href="#">Reviews</a></li>
+                        <li><a href="#">Blog</a></li>
+                        <li><a href="#">Support</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="col-lg-2">
+                <a href="#" class="button top_button">Get App</a>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-7">
+                <img src="img/apple_icon.png" alt="">
+                <h1>Helps you eat better.</h1>
+                <p>FitApp is a free calorie counting and diet app PSD template which includes high quality 9 screens. This template can be very helpful if you're planning to build food tracking application.</p>
+            </div>
+            <div class="col-lg-5"></div>
+        </div>
+    </div>
+</header>
+</body>
+</html>
