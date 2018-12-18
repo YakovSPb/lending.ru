5d29fe707e31c7b6da924ed947e4a5c9372a7290
Yakov <yakov_dux@bk.ru>
2018-12-18 20:30:02 +0400

    Done

--- index.php
+++ index.php
@@ -6,43 +6,245 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Lending</title>
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
     <!-- CSS -->
-    <link type="text/css" rel="stylesheet" href="css/main.css">
     <link type="text/css" rel="stylesheet" href="css/bootstrap.min.css">
+     <link type="text/css" rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link type="text/css" rel="stylesheet" href="css/main.css">
+
+    <!-- Bootstrap CSS -->
 </head>
 <body>
 <header>
-    <div class="container">
+    <div class="banana"></div>
+    <div class="tomato"></div>
+    <div class="apple"></div>
+    <div class="apple2"></div>
+    <div class="strawberries"></div>
+      <div class="container">
         <div class="row">
-            <div class="col-lg-2">
-                <div class="logo">FitApp</div>
-            </div>
-             <div class="col-lg-6 offset-lg-2">
-                <nav>
-                    <ul class="top_menu">
-                        <li><a href="#">About</a></li>
-                        <li><a href="#">Features</a></li>
-                        <li><a href="#">Reviews</a></li>
-                        <li><a href="#">Blog</a></li>
-                        <li><a href="#">Support</a></li>
-                    </ul>
+            <div class="col">
+                <nav class="navbar navbar-expand-lg navbar-dark">
+                    <div class="logo animated fadeInDown">FitApp</div>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="top_menu collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                            <li class="nav-item"><a class="nav-link" href="#">About</a></li>
+                            <li class="nav-item"><a class="nav-link" href="#">Features</a></li>
+                            <li class="nav-item"><a class="nav-link" href="#">Reviews</a></li>
+                            <li class="nav-item"><a class="nav-link" href="#">Blog</a></li>
+                            <li class="nav-item"><a class="nav-link" href="#">Support</a></li>
+                            <li class="nav-item"><a href="#" class="nav-link button top_button animated fadeInDown">Get App</a></li>
+                        </ul>
+                    </div>
                 </nav>
             </div>
-            <div class="col-lg-2">
-                <a href="#" class="button top_button">Get App</a>
-            </div>
         </div>
-    </div>
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-7">
-                <img src="img/apple_icon.png" alt="">
-                <h1>Helps you eat better.</h1>
-                <p>FitApp is a free calorie counting and diet app PSD template which includes high quality 9 screens. This template can be very helpful if you're planning to build food tracking application.</p>
+
+        <div class="header_body">
+            <div class="row">
+                <div class="col">
+                    <div class="header_apple_icon">
+                        <img src="img/apple_icon.png" class="animated fadeInLeft" alt="Hey dude">
+                    </div>
+                    <h1>Helps you eat <strong>better</strong>.</h1>
+                    <p>FitApp is a free calorie counting and diet app PSD template which includes high quality 9 screens. This template can be very helpful if you're planning to build food tracking application.</p>
+                    <div class="header_icon">
+                        <a href="#"><img src="img/appstore_icon.png" alt=""></a>
+                        <a href="#"><img src="img/google_icon.png" alt=""></a>
+                    </div>
+                </div>
+                <div class="col-5 d-none d-lg-block">
+                    <div class="header__phone">
+
+                    </div>
+                </div>
             </div>
-            <div class="col-lg-5"></div>
+
         </div>
+
     </div>
 </header>
+<section class="counter-app">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 col-lg-6">
+                <img class="counter-app__phone-img d-none d-lg-block" src="img/main_phone.png" alt="counter app for phone">
+                <div class="curve">
+                    <div class="curve-icon-wrapper">
+                        <div class="curve-icon sprite-sound"></div>
+                    </div>
+                    <div class="curve-icon-wrapper">
+                        <div class="curve-icon sprite-days"></div>
+                    </div>
+                    <div class="curve-icon-wrapper">
+                        <div class="curve-icon sprite-growth"></div>
+                    </div>
+                    <div class="curve-icon-wrapper">
+                        <div class="curve-icon sprite-favorite"></div>
+                    </div>
+                    <div class="curve-icon-wrapper">
+                        <div class="curve-icon sprite-interface"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-offset-6 col-lg-6 col-md-offset-5 col-md-6 col-sm-offset-1 col-sm-10 col-xs-12">
+                <div class="feature-desc animated fadeIn">
+                    <h2 class="feature-desc__name">Best calorie <span class="green-text">counter app!</span></h2>
+                    <p class="feature-desc__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                        et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                </div>
+                <div class="feature-desc animated fadeIn">
+                    <h2 class="feature-desc__name">2Best calorie <span class="green-text">counter app!</span></h2>
+                    <p class="feature-desc__text">2Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                        et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                </div>
+                <div class="feature-desc animated fadeIn">
+                    <h2 class="feature-desc__name">3Best calorie <span class="green-text">counter app!</span></h2>
+                    <p class="feature-desc__text">3Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                        et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                </div>
+                <div class="feature-desc animated fadeIn">
+                    <h2 class="feature-desc__name">4Best calorie <span class="green-text">counter app!</span></h2>
+                    <p class="feature-desc__text">4Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                        et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                </div>
+                <div class="feature-desc animated fadeIn">
+                    <h2 class="feature-desc__name">5Best calorie <span class="green-text">counter app!</span></h2>
+                    <p class="feature-desc__text">5Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                        et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="simple_way">
+
+    <div class="container">
+        <div class="row">
+            <div class="col">
+            <h2>A simple way to <span class="green-text">plan your diet!</span></h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et<br/> dolore magna aliqua. Ut enim ad minim veniam.</p>
+            </div>
+        </div>
+
+
+        <div class="row">
+            <div class="col">
+                <div class="one_phone">
+                </div>
+                <div class="two_phone">
+
+                </div>
+                <div class="three_phone">
+
+                </div>
+
+
+            </div>
+
+
+        </div>
+
+    </div>
+
+</section>
+<section class="manage">
+     <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="manage-content m-apple animated fadeInLeft">
+                    <h2>1 Manage your <span class="green-text">meal time</span></h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                </div>
+                <div class="manage-content m-sandwich animated fadeInLeft">
+                    <h2>2 Manage your <span class="green-text">meal time</span></h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                </div>
+                <div class="manage-content m-salad animated fadeInLeft">
+                    <h2>3 Manage your <span class="green-text">meal time</span></h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                </div>
+
+            </div>
+            <div class="col d-none d-lg-block">
+                <div class="carousel_fruit">
+                <div class="carousel__apple">
+                    <div class="carousel__up">
+                        <div class="carousel_left">
+                        <strong>Apple</strong>
+                           <p>Lorem ipsum dolor sit</p>
+                            <span>35 G <i class="fa fa-circle" aria-hidden="true"></i> 189 KCAL<span>
+                        </div>
+                        <div class="clock">
+                            <strong>01:00</strong>
+                            PM
+                        </div>
+                        </div>
+                    </div>
+                    <div class="carousel__sandwich">
+                        <div class="carousel__up">
+                            <div class="carousel_left">
+                                <strong>Sandwich</strong>
+                                <p>Lorem ipsum dolor sit</p>
+                                <span>35 G <i class="fa fa-circle" aria-hidden="true"></i> 189 KCAL<span>
+                            </div>
+                            <div class="clock">
+                                <strong>01:00</strong>
+                                PM
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel__salad">
+                        <div class="carousel__up">
+                            <div class="carousel_left">
+                                <strong>Salad with chicken</strong>
+                                <p>Lorem ipsum dolor sit</p>
+                                <span>35 G <i class="fa fa-circle" aria-hidden="true"></i> 189 KCAL<span>
+                            </div>
+                            <div class="clock">
+                                <strong>01:00</strong>
+                                PM
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+
+<footer>
+<div class="container">
+    <div class="fruit d-none d-lg-block">
+        <div class="banana2"></div>
+        <div class="tomato2"></div>
+        <div class="apple3"></div>
+        <div class="paprika2"></div>
+        <div class="strawberries2"></div>
+    </div>
+        <div class="row">
+
+            <h2>Download, customize & <span>enjoy!</span></h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+            </p>
+            <a href="#" class="button b-footer">Get App</a>
+        </div>
+
+</div>
+
+    </div>
+</footer>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/main.js"></script>
+<script src="js/carousel.js"></script>
 </body>
 </html>
